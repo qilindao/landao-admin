15e53177ace46aebc0abe1a7c665424cddd7c576
joycezhang <787027175@qq.com>
2022-07-18 15:04:10 +0800

    ♻️(LdTable): update

--- src/components/LdTable/hooks/useDataSource.js
+++ src/components/LdTable/hooks/useDataSource.js
@@ -1,89 +1,93 @@
 import { isFunction } from "@/landao/utils/is";
-import { isBoolean } from "lodash-es";
+import { isBoolean, omit } from "lodash-es";
 import { ref, unref, onMounted, computed } from "vue";
 
-export function useDataSource(propsRef, {
-    setLoading,
-    setPaginationTotal,
-    getPaginationInfo
-}) {
-    //数据源
-    const dataSourceRef = ref([])
+export function useDataSource(
+  propsRef,
+  { setLoading, setPaginationTotal, getPaginationInfo }
+) {
+  //数据源
+  const dataSourceRef = ref([]);
 
-    const getDataSourceRef = computed(() => {
-        return unref(dataSourceRef)
-    })
+  const getDataSourceRef = computed(() => {
+    return unref(dataSourceRef);
+  });
 
-    /**
-     * 设置数据源
-     * @param {*} values 
-     */
-    function setDataSource(values) {
-        dataSourceRef.value = values
+  /**
+   * 设置数据源
+   * @param {*} values
+   */
+  function setDataSource(values) {
+    dataSourceRef.value = values;
+  }
+  /**
+   * 获取api数据
+   * @param {Object} params
+   * @returns
+   */
+  async function fetch(params = {}) {
+    const { api, afterFetch, pagination } = unref(propsRef);
+    if (!api || !isFunction(api)) return;
+    let pageParams = {};
+    const { currentPage = 1, pageSize = 20 } = unref(getPaginationInfo);
+    //如果没有分页，不传分页参数
+    if ((isBoolean(pagination) && pagination) || isBoolean(getPaginationInfo)) {
+      pageParams["page"] = (params && params.page) || currentPage;
+      pageParams["page_size"] = pageSize;
     }
-    /**
-     * 获取api数据
-     * @param {Object} params 
-     * @returns 
-     */
-    async function fetch(params = {}) {
-        const { api, afterFetch, pagination } = unref(propsRef)
-        if (!api || !isFunction(api)) return;
-        let pageParams = {};
-        const { currentPage = 1, pageSize = 20 } = unref(getPaginationInfo);
-        //如果没有分页，不传分页参数
-        if ((isBoolean(pagination) && pagination) || isBoolean(getPaginationInfo)) {
-            pageParams['page'] = (params && params.page) || currentPage;
-            pageParams['page_size'] = pageSize;
+    try {
+      setLoading(true);
+      //请求
+      const res = await api({
+        ...pageParams,
+        ...omit(params, ["page", "page_size"]),
+      });
+      console.log("dataSource", res.data);
+      const isArrayResult = Array.isArray(res.data);
+      console.log(isArrayResult);
+      let resultItem = [];
+      if (isArrayResult) {
+        resultItem = res.data;
+      } else {
+        if (pagination) {
+          //设置总条数
+          setPaginationTotal(res.data.pagination.total);
         }
-
-
-        try {
-            setLoading(true)
-            //请求
-            const res = await api({ ...pageParams, ...params });
-            const isArrayResult = Array.isArray(res.data);
-            let resultItem = [];
-            if (isArrayResult) {
-                resultItem = res.data;
-            } else {
-                if (pagination) {
-                    //设置总条数
-                    setPaginationTotal(data.pagination.total)
-                }
-                resultItem = res.data.list;
-            }
-            //对返回值进行处理，比如将数据转成树形数据，或对加密数据进行解密或脱敏
-            if (afterFetch && isFunction(afterFetch)) {
-                resultItem = (await afterFetch(resultItem)) || resultItem
-            }
-            setDataSource(resultItem)
-        } catch (error) {
-            setDataSource([])
-            setPaginationTotal(0)
-        } finally {
-            setLoading(false)
-        }
+        console.log("resultItem", res.data.list);
+        resultItem = res.data.list;
+      }
+      //对返回值进行处理，比如将数据转成树形数据，或对加密数据进行解密或脱敏
+      if (afterFetch && isFunction(afterFetch)) {
+        resultItem = (await afterFetch(resultItem)) || resultItem;
+      }
+      setDataSource(resultItem);
+    } catch (error) {
+      setDataSource([]);
+      setPaginationTotal(0);
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    //刷新表格
-    async function reload(params) {
-        return await fetch(params)
-    }
+  //刷新表格
+  async function reload(params) {
+    return await fetch(params);
+  }
 
-    //获取数据
-    function getDataSource() {
-        return getDataSource.value || []
-    }
+  //获取数据
+  function getDataSource() {
+    return getDataSource.value || [];
+  }
 
-    onMounted(() => {
-        fetch()
-    })
+  onMounted(() => {
+    fetch();
+  });
 
-    return {
-        getDataSourceRef,
-        getDataSource,
-        fetch,
-        reload
-    }
+  return {
+    getDataSourceRef,
+    getDataSource,
+    fetch,
+    reload,
+  };
 }
