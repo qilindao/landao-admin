769023be6693b33e3907d0da0a5d98eabb96f46d
joycezhang <787027175@qq.com>
2022-07-19 16:53:35 +0800

    ♻️(update):

--- vite/plugins/importToCDN.js
+++ vite/plugins/importToCDN.js
@@ -9,43 +9,48 @@ import importToCDN from 'vite-plugin-cdn-import'
 export default function createImportToCDN(env) {
 
     const { VITE_IS_IMPORT_TO_CDN } = env;
-    const cndUrl = '//unpkg.com';
+    const cdnUrl = '//unpkg.com';
     const cdns = [
         {
             name: 'vue',
             var: 'Vue',
-            path: `${cndUrl}/vue@3.2.33/dist/vue.runtime.global.prod.js`
+            path: `${cdnUrl}/vue@3.2.33/dist/vue.runtime.global.prod.js`
         },
         {
             name: 'vue-router',
             var: 'VueRouter',
-            path: `${cndUrl}/vue-router@4.0.15/dist/vue-router.global.prod.js`
+            path: `${cdnUrl}/vue-router@4.0.15/dist/vue-router.global.prod.js`
         },
         {
             name: 'axios',
             var: 'axios',
-            path: `${cndUrl}/axios@0.27.2/dist/axios.min.js`
+            path: `${cdnUrl}/axios@0.27.2/dist/axios.min.js`
         },
         {
             name: 'dayjs',
             var: 'dayjs',
-            path: `${cndUrl}/dayjs@1.11.2/dayjs.min.js`
+            path: `${cdnUrl}/dayjs@1.11.2/dayjs.min.js`
         },
         {
             name: 'element-plus',
             var: 'ElementPlus',
-            path: `${cndUrl}/element-plus@2.2.0`,
-            css: `${cndUrl}/element-plus@2.2.0/dist/index.css`
+            path: `${cdnUrl}/element-plus@2.2.0`,
+            css: `${cdnUrl}/element-plus@2.2.0/dist/index.css`
         },
         {
             name: '@element-plus/icons-vue',
             var: 'IconsVue',
-            path: `${cndUrl}/@element-plus/icons-vue@2.0.4`,
+            path: `${cdnUrl}/@element-plus/icons-vue@2.0.4`,
+        },
+        {
+            name: '@vueuse/core',
+            var: 'VueUse',
+            path: `${cdnUrl}/@vueuse/core`,
         },
         {
             name: 'echarts',
             var: 'echarts',
-            path: `${cndUrl}/echarts@5.3.3/dist/echarts.min.js`,
+            path: `${cdnUrl}/echarts@5.3.3/dist/echarts.min.js`,
         }
     ];
 
